38ceadd85ef7281e818ca589ba774500ee3c0b2e
schwann2402 <139656534+schwann2402@users.noreply.github.com>
2025-01-19 18:44:09 -0500

    minor typo fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: latest,
+    ecmaVersion: "latest",
     sourceType: "module",
   },
   plugins: ["react", "jest"],
