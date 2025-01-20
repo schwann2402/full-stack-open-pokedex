28cc5ff6962921602b8c1f9c6e5ca17e979605b5
schwann2402 <139656534+schwann2402@users.noreply.github.com>
2025-01-19 22:05:27 -0500

    ...

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  this_causes_error
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
 
 app.get('/version', (req, res) => {
   res.send('2') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
