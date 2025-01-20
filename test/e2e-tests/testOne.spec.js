ad9aecd72254cfbc7a95b7c6cd223af103661dfd
schwann2402 <139656534+schwann2402@users.noreply.github.com>
2025-01-19 21:24:19 -0500

    e2e tests

---
+++ test/e2e-tests/testOne.spec.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import { test, describe, expect, _beforeEach } from '@playwright/test'
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+})
