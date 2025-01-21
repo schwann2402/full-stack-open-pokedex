2a5ddda625c82503130b0d514383e26586a33f0e
schwann2402 <139656534+schwann2402@users.noreply.github.com>
2025-01-20 20:47:37 -0500

    trying build

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  dmskdmskmdkadsadaadsadsadasdas
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
